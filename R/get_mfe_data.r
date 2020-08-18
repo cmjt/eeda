eef480d78abea0c775b201930462e32f1773044a
cmjt <cmjt@st-andrews.ac.uk>
2020-08-18 17:05:16 +1200

    downloading mfe data

--- R/get_mfe_data.r
+++ R/get_mfe_data.r
@@ -1,54 +1,53 @@
-#' Function to see available mfe spatial layer datasets available to
+#' Function to exploe all available mfe spatial layer datasets available to
 #' download
-#' @param key API key for \url{http://data.mfe.govt.nz}
 #' @export
-mfe_data <- function(key) {
-    url <- paste("https://data.mfe.govt.nz/services;key=", key,
-                 "/wfs?service=WFS&request=GetCapabilities", sep = "")
-    pg <- xml2::read_html(url)
-    titles <- pg %>% rvest::html_nodes("title") %>% rvest::html_text()
-    abstracts <- pg %>% rvest::html_nodes("abstract") %>% rvest::html_text()
-    layerid <- pg %>% rvest::html_nodes("name") %>% rvest::html_text()
-    res <- data.frame(Title = titles[-1], Abstract = abstracts[-1],
-                      Layerid = layerid)
-    ley <- grep("layer", res[, 3])
-    return(res[ley, ])
+mfe_data <- function() {
+    url <- "https://data.mfe.govt.nz/layers/?v=rows"
+    pg <- read_html(url)
+    ## get the total number of items
+    items <- as.numeric(
+        strsplit(
+        (pg %>%
+         html_nodes("span.listPagerHeaderTitle") %>%
+         html_text())[2],"of ")[[1]][2])
+    num.pages <- ceiling(items/30)
+    ## all page urls
+    urls <- paste("https://data.mfe.govt.nz/layers/?page=",1:num.pages,"&v=rows", sep = "")
+    ## read all pages
+    pages <- lapply(urls, xml2::read_html)
+    titles <- lapply(pages, function(x) (x %>% rvest::html_table())[[2]][,1])
+    titles <- unlist(titles)
+    layers <- lapply(pages, function(x) x %>%
+                                        rvest::html_nodes(".trigger-download-dialog")  %>%
+                                        rvest::html_attr("data-item-id"))
+    layers <- unlist(layers)
+    lay.nums <- sapply(sapply(layers, strsplit, "layer."), function(x) x[2])
+    rownames(lay.nums) <- NULL
+    return(data.frame(Data = titles,"Layer number" = lay.nums))
 }
-#' Function to get specific mfe spatial layer ID
-#' @inheritParams mfe_data
-#' @param x a character string to search mfe datasets
-#' @return a vector of mfe dataset ids
-#' @export
-get_layer_id <- function(x, key) {
-    abs <- mfe_data(key = key)
-    idx <- unique(grep(x, abs[, 2]))
-    id <- sub(".*:", "", abs[idx, 3])
-    n <- length(idx)
-    cat(paste(n, "results found"), "\n")
-    res <- data.frame(id = id); rownames(res) <- abs[idx, 1]
-    return(res)
-}
+
 #' Function to download a specified spatial layer from
 #' \url{http://data.mfe.govt.nz}
 #' @param id layer ID of mfe dataset
-#' @inheritParams mfe_data
-#' @param plot Logical. If TRUE will plot data
+#' @param key API key for \url{https://data.mfe.govt.nz/}
 #' @export
-get_mfe_data <- function(id, key, plot = FALSE) {
+get_mfe_data <- function(id, key) {
     if(length(id) > 1) {
         print("id should be of length 1, using first element only")
         id <- id[1]
     }
-    urlp1 <- paste("https://data.mfe.govt.nz/services;key=", key, sep = "")
-    urlp2 <- paste("/wfs?service=WFS&version=2.0.0&request=GetFeature&typeNames=",
-                   id, "&outputFormat=KML", sep = "")
-    url <- paste(urlp1, urlp2, sep = "")
-    tmp <- tempfile(pattern = id, tmpdir = "./", fileext = ".kml")
-    download.file(url, destfile = tmp)
-    lay <- rgdal::ogrListLayers(tmp)
-    print(lay)
-    res <- rgdal::readOGR(tmp, layer = lay)
-    unlink(tmp)
-    if(plot) show_nz_river(res)
+    id <- paste("layer-",id, sep = "")
+    base_url <- "https://data.mfe.govt.nz"
+    endpoint <- paste("/services;key=",key, "/wfs",sep = "")
+    q <- list(request = "GetFeature", service = "WFS", typeNames = id,outputFormat = "KML")
+    file <- tempfile(fileext = ".kml")
+    httr::GET(url = base_url, path = endpoint, query = q, httr::write_disk(file))
+    lay <- rgdal::ogrListLayers(file)
+    res <- rgdal::readOGR(file, layer = lay)
+    unlink(file)
     return(res)
 }
+
+#' @importFrom magrittr %>%
+#' @importFrom xml2 read_html
+#' @importFrom rvest html_nodes html_text
